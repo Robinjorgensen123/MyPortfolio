9f2fe427cb7c5a85b6b3b73d49dca15a3b9668ce
robjor88 <robjor88@gmail.com>
2025-10-18 10:06:21 +0200

    added github

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,9 @@
 const About: React.FC = () => (
     <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
     <h1 className="text-3xl font-bold">Om mig</h1>
-    <p className="mt-4 max-w-2xl text-zinc-300">BIO...</p>
+    <p className="mt-4 max-w-2xl text-zinc-300">Jag är en nyfiken och målmedveten JavaScript-utvecklare under utbildning.
+I mitt arbete gillar jag att kombinera kreativitet och logik — oavsett om det handlar om att bygga användarvänliga frontend-gränssnitt med React eller att skapa effektiva API:er i Node.js.
+Jag lär mig snabbt och gillar att förstå varför saker fungerar, inte bara hur.</p>
   </section>
 )
 
